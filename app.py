51db78688ac85d079ae027b8507eacd3c41d45f4
Austin <austin.lazarus@gmail.com>
2016-07-10 18:08:34 -0400

    10.07.16

--- app.py
+++ app.py
@@ -8,6 +8,24 @@ kik = KikApi("Suggestionbot", "de3b3ecd-f085-44a0-9103-f899467ecdf4")
 kik.set_configuration(Configuration(webhook="https://calm-caverns-74991.herokuapp.com/incoming"))
 
 #**********************************************************#
+# // Kik send.message global function w/
+# // keyboards argument 
+#**********************************************************#
+def send_text(user, chat_id, body, keyboards=[]):
+    """Send text."""
+    message = TextMessage(to=user, chat_id=chat_id, body=body)
+    if keyboards:
+        message.keyboards.append(
+            SuggestedResponseKeyboard(
+                to=user,
+                hidden=False,
+                responses=[TextResponse(keyboard) for keyboard in keyboards],
+            )
+        )
+    kik.send_messages([message])
+
+
+#**********************************************************#
 # // INCOMING ROUTE //
 #**********************************************************#
 @app.route('/incoming', methods=['POST'])
@@ -18,6 +36,7 @@ def incoming():
     messages = messages_from_json(request.json['messages'])
     for message in messages:
         if isinstance(message, TextMessage):
+
             '''
             kik.send_messages([
                 TextMessage(
@@ -25,10 +44,10 @@ def incoming():
                     chat_id=message.chat_id,
                     body=message.body) ])
             '''
-                if 'hi' in message.body.lower() or 'hello' in message.body.lower():
-                    text = 'Hi {}!'.format(message.from_user)
-                else:
-                    text = 'I don\'t understand message'
+            if 'hi' in message.body.lower() or 'hello' in message.body.lower():
+                text = 'Hi {}!'.format(message.from_user)
+            else:
+                text = 'I don\'t understand message'
                 send_text(message.from_user, message.chat_id, text)
 
     return Response(status=200)
